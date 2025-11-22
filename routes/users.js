f9e639ef7351df3c603fe48f34b4c775c26e0c7e
NightFlash96 <nightflash94@gmail.com>
2025-11-22 21:31:24 +0000

    added audit log

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,17 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
+// function auditLog(username, success) {
+//     db.query("INSERT INTO audit (username, datetime, success) VALUES (?, NOW(), ?)", [username, success], (err, result) => {
+//         if (err) {
+//             next(err)
+//         }
+//         else {
+//             console.log('Failed login attempt logged. Wrong username/email.')
+//         }
+//     });
+// }
+
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 });
@@ -57,28 +68,66 @@ router.get('/login', function (req, res, next) {
 });
 
 router.post('/loggedin', function (req, res, next) {
-    let sqlquery = "SELECT hashedPassword FROM users WHERE username= ? OR email= ?";
+    let sqlquery = "SELECT hashedPassword FROM users WHERE username= ? OR email= ?"; // query database to get hashed password for the username/email
     // execute sql query
     db.query(sqlquery, [req.body.username, req.body.username], (err, result) => {
         if (err) {
             next(err)
         }
-        else if (result.length === 0) {
+        else if (result.length === 0) { // if the username or email does not exist, send error message and log failed attempt
             res.send('Username or email not found.')
             console.log(result);
+            db.query("INSERT INTO audit (username, datetime, success, eventType) VALUES (?, NOW(), 0, 'incorrect username/email')", [req.body.username], (err, result) => {
+                if (err) {
+                    next(err)
+                }
+                else {
+                    console.log('Failed login attempt logged. Incorrect username/email.')
+                }
+            });
         }
-        else {
+        else { // else hash and compare the submitted password to the stored hashed password
             bcrypt.compare(req.body.password, result[0].hashedPassword, function(err, result) {
                 if (err) {
                     next (err)
                 }
-                else if (result == true) {
+                else if (result == true) { // if match, log successful attempt
                     res.send('You are now logged in!')
+                    db.query("INSERT INTO audit (username, datetime, success, eventType) VALUES (?, NOW(), 1, 'login')", [req.body.username], (err, result) => {
+                        if (err) {
+                            next(err)
+                        }
+                        else {
+                            console.log('Successful login attempt logged.')
+                        }
+                    });
                 }
-                else {
-                    res.send('Login failed')
+                else { // if the password does not match, log failed attempt due to incorrect password
+                    res.send('Login failed, incorrect password.')
+                    db.query("INSERT INTO audit (username, datetime, success, eventType) VALUES (?, NOW(), 0, 'incorrect password')", [req.body.username], (err, result) => {
+                        if (err) {
+                            next(err)
+                        }
+                        else {
+                            console.log('Failed login attempt logged. Incorrect password.')
+                        }
+                    });
                 }
             });    
+        }
+    });
+});
+
+router.get('/audit', function(req, res, next) {
+    let sqlquery = "SELECT * FROM audit"; // query database to get all the audit logs
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        else {
+            console.log(result);
+            res.render("auditlog.ejs", {auditlogs: result})
         }
     });
 });
